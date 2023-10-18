34e2fa246f1fc36cebd193dedb4facebbc253005
Santiago Baliño <santiagobalino@hotmail.com>
2023-10-18 14:29:00 -0300

    first commit

---
+++ exercise.js
@@ -0,0 +1,69 @@
+const puertos = [
+    [0, 0, 0, 0, 0],
+    [11, 11, 11, 11, 11, 11],
+    [0, 0, 0, 0, 0, 0, 0],
+];
+let interval;
+let flagForNext = false;
+let random = 500;
+let nextItem = 0;
+let currentArr = 0;
+
+const mapPorts = (ports, puerto) => {
+    if (ports.includes(0)) {
+        ports.map((_, index) => {
+            if (index === nextItem) {
+                console.log(`${puerto}: Iteración número: ${index}`);
+            }
+        });
+        flagForNext = false;
+        nextItem = nextItem + 1;
+        if (nextItem === ports.length) {
+            currentArr = currentArr + 1;
+            nextItem = 0;
+        }
+    } else {
+        flagForNext = false;
+        currentArr = currentArr + 1;
+    }
+    if (currentArr === puertos.length) {
+        flagForNext = false;
+        currentArr = 0;
+    }
+
+    clearInterval(interval);
+    random = Math.floor(Math.random() * 1000);
+    getPorts(puertos, random);
+};
+
+const getPuertos = (puertos) => {
+    let a = "PRIMER IP 11";
+    let b = "SEGUNDO IP 12";
+    let c = "TERCER IP 14";
+    flagForNext = random >= 400 && random <= 600 ? true : false;
+
+    puertos.map((puerto, index) => {
+        if (flagForNext) {
+            if (index === currentArr) {
+                if (currentArr === 0) {
+                    mapPorts(puerto, a);
+                } else if (currentArr === 1) {
+                    mapPorts(puerto, b);
+                } else if (currentArr === 2) {
+                    mapPorts(puerto, c);
+                }
+            }
+        }
+    });
+    clearInterval(interval);
+    random = Math.floor(Math.random() * 1000);
+    getPorts(puertos, random);
+};
+
+const getPorts = (puertos, timer) => {
+    interval = setInterval(() => {
+        getPuertos(puertos);
+    }, timer);
+};
+
+getPorts(puertos, random);
